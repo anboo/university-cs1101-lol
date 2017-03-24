2ae32c6894caea2e6612dfcd9234ad36ebd63c3f
anboo <admin@rusdteam.ru>
2017-03-24 13:40:44 +0300

    refactor math operation sytax

--- first.rkt
+++ first.rkt
@@ -1,23 +1,28 @@
 #lang racket
 (require 2htdp/image) 
 
-(string-append "what a " "lively " "day " "4 BSL")
-
 (define prefix "hello")
 (define suffix "world")
 
-(+ 
+(
+ + 
  (string-length (string-append prefix "_" suffix)) 
  20
 )
 
-(+ 
+(
+ + 
  (string-length (number->string 341)) 
  (string->number "20")
 )
 
 (define x 2)
-(if (= x 0) 0 (/ 1 x))
+(
+ if (= x 0) 
+    0 
+    (/ 1 x)
+)
+
 x
 
 (define (ff wrong_string)
