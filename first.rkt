bc3b8a7d7f317f2945375196c5c2db587c8e59da
anboo <admin@rusdteam.ru>
2017-03-24 10:14:02 +0300

    add function strlen by dinamic type

--- first.rkt
+++ first.rkt
@@ -1,14 +1,29 @@
 #lang racket
-(define x 3)
-(define y 4)
+(require 2htdp/image) 
 
-(+ x y)
-(* x y)
- 
 (string-append "what a " "lively " "day " "4 BSL")
 
 (define prefix "hello")
 (define suffix "world")
 
-(+ (string-length (string-append prefix "_" suffix)) 20)
-(+ (string-length (number->string 341)) 20)
+(+ 
+ (string-length (string-append prefix "_" suffix)) 
+ 20
+)
+
+(+ 
+ (string-length (number->string 341)) 
+ (string->number "20")
+)
+
+(define x 2)
+(if (= x 0) 0 (/ 1 x))
+x
+
+(define (ff wrong_string)
+  (
+   if (string? wrong_string) (string-length wrong_string) (string-length (number->string wrong_string))
+  ))
+
+(ff "1234567")
+(ff 1234567)
